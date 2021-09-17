1233a01f73c4afc957c2c2a5db9e6d8b1b04127a
codybravo <armjacob1@gmail.com>
2021-09-18 03:06:57 +0530

    all feature is working

---
+++ background.js
@@ -0,0 +1,162 @@
+// charArray["𝒜"] = charArray["𝗔"] = charArray["𝘈"] = charArray["𝘈"] =
+// charArray["ℬ"] = charArray["𝗕"] = charArray["𝘉"] = charArray["𝘉"] =
+// charArray["𝒞"] = charArray["𝗖"] = charArray["𝘊"] = charArray["𝘊"] =
+// charArray["𝒟"] = charArray["𝗗"] = charArray["𝘋"] = charArray["𝘋"] =
+// charArray["ℰ"] = charArray["𝗘"] = charArray["𝘌"] = charArray["𝘌"] =
+// charArray["ℱ"] = charArray["𝗙"] = charArray["𝘍"] = charArray["𝘍"] =
+// charArray["𝒢"] = charArray["𝗚"] = charArray["𝘎"] = charArray["𝘎"] =
+// charArray["ℋ"] = charArray["𝗛"] = charArray["𝘏"] = charArray["𝘏"] =
+// charArray["ℐ"] = charArray["𝗜"] = charArray["𝘐"] = charArray["𝘐"] =
+// charArray["𝒥"] = charArray["𝗝"] = charArray["𝘑"] = charArray["𝘑"] =
+// charArray["𝒦"] = charArray["𝗞"] = charArray["𝘒"] = charArray["𝘒"] =
+// charArray["ℒ"] = charArray["𝗟"] = charArray["𝘓"] = charArray["𝘓"] =
+// charArray["ℳ"] = charArray["𝗠"] = charArray["𝘔"] = charArray["𝘔"] =
+// charArray["𝒩"] = charArray["𝗡"] = charArray["𝘕"] = charArray["𝘕"] =
+// charArray["𝒪"] = charArray["𝗢"] = charArray["𝘖"] = charArray["𝘖"] =
+// charArray["𝒫"] = charArray["𝗣"] = charArray["𝘗"] = charArray["𝘗"] =
+// charArray["𝒬"] = charArray["𝗤"] = charArray["𝘘"] = charArray["𝘘"] =
+// charArray["ℛ"] = charArray["𝗥"] = charArray["𝘙"] = charArray["𝘙"] =
+// charArray["𝒮"] = charArray["𝗦"] = charArray["𝘚"] = charArray["𝘚"] =
+// charArray["𝒯"] = charArray["𝗧"] = charArray["𝘛"] = charArray["𝘛"] =
+// charArray["𝒰"] = charArray["𝗨"] = charArray["𝘜"] = charArray["𝘜"] =
+// charArray["𝒱"] = charArray["𝗩"] = charArray["𝘝"] = charArray["𝘝"] =
+// charArray["𝒲"] = charArray["𝗪"] = charArray["𝘞"] = charArray["𝘞"] =
+// charArray["𝒳"] = charArray["𝗫"] = charArray["𝘟"] = charArray["𝘟"] =
+// charArray["𝒴"] = charArray["𝗬"] = charArray["𝘠"] = charArray["𝘠"] =
+// charArray["𝒵"] = charArray["𝗭"] = charArray["𝘡"] = charArray["𝘡"] =
+// charArray["𝒶"] = charArray["𝗮"] = charArray["𝘢"] = charArray["𝘢"] =
+// charArray["𝒷"] = charArray["𝗯"] = charArray["𝘣"] = charArray["𝘣"] =
+// charArray["𝒸"] = charArray["𝗰"] = charArray["𝘤"] = charArray["𝘤"] =
+// charArray["𝒹"] = charArray["𝗱"] = charArray["𝘥"] = charArray["𝘥"] =
+// charArray["ℯ"] = charArray["𝗲"] = charArray["𝘦"] = charArray["𝘦"] =
+// charArray["𝒻"] = charArray["𝗳"] = charArray["𝘧"] = charArray["𝘧"] =
+// charArray["ℊ"] = charArray["𝗴"] = charArray["𝘨"] = charArray["𝘨"] =
+// charArray["𝒽"] = charArray["𝗵"] = charArray["𝘩"] = charArray["𝘩"] =
+// charArray["𝒾"] = charArray["𝗶"] = charArray["𝘪"] = charArray["𝘪"] =
+// charArray["𝒿"] = charArray["𝗷"] = charArray["𝘫"] = charArray["𝘫"] =
+// charArray["𝓀"] = charArray["𝗸"] = charArray["𝘬"] = charArray["𝘬"] =
+// charArray["𝓁"] = charArray["𝗹"] = charArray["𝘭"] = charArray["𝘭"] =
+// charArray["𝓂"] = charArray["𝗺"] = charArray["𝘮"] = charArray["𝘮"] =
+// charArray["𝓃"] = charArray["𝗻"] = charArray["𝘯"] = charArray["𝘯"] =
+// charArray["ℴ"] = charArray["𝗼"] = charArray["𝘰"] = charArray["𝘰"] =
+// charArray["𝓅"] = charArray["𝗽"] = charArray["𝘱"] = charArray["𝘱"] =
+// charArray["𝓆"] = charArray["𝗾"] = charArray["𝘲"] = charArray["𝘲"] =
+// charArray["𝓇"] = charArray["𝗿"] = charArray["𝘳"] = charArray["𝘳"] =
+// charArray["𝓈"] = charArray["𝘀"] = charArray["𝘴"] = charArray["𝘴"] =
+// charArray["𝓉"] = charArray["𝘁"] = charArray["𝘵"] = charArray["𝘵"] =
+// charArray["𝓊"] = charArray["𝘂"] = charArray["𝘶"] = charArray["𝘶"] =
+// charArray["𝓋"] = charArray["𝘃"] = charArray["𝘷"] = charArray["𝘷"] =
+// charArray["𝓌"] = charArray["𝘄"] = charArray["𝘸"] = charArray["𝘸"] =
+// charArray["𝓍"] = charArray["𝘅"] = charArray["𝘹"] = charArray["𝘹"] =
+// charArray["𝓎"] = charArray["𝘆"] = charArray["𝘺"] = charArray["𝘺"] =
+// charArray["𝓕"] = charArray["𝘇"] = charArray["𝘻"] = charArray["𝘻"] =
+// charArray["1"] = charArray["𝟭"] = charArray["1"] = charArray["1"] =
+// charArray["2"] = charArray["𝟮"] = charArray["2"] = charArray["2"] =
+// charArray["3"] = charArray["𝟯"] = charArray["3"] = charArray["3"] =
+// charArray["4"] = charArray["𝟰"] = charArray["4"] = charArray["4"] =
+// charArray["5"] = charArray["𝟱"] = charArray["5"] = charArray["5"] =
+// charArray["6"] = charArray["𝟲"] = charArray["6"] = charArray["6"] =
+// charArray["7"] = charArray["𝟳"] = charArray["7"] = charArray["7"] =
+// charArray["8"] = charArray["𝟴"] = charArray["8"] = charArray["8"] =
+// charArray["9"] = charArray["𝟵"] = charArray["9"] = charArray["9"] =
+// charArray["0"] = charArray["𝟬"] = charArray["0"] = charArray["0"] =
+// "𝒜"
+// "ℬ"
+// "𝒞"
+// "𝒟"
+// "ℰ"
+// "ℱ"
+// "𝒢"
+// "ℋ"
+// "ℐ"
+// "𝒥"
+// "𝒦"
+// "ℒ"
+// "ℳ"
+// "𝒩"
+// "𝒪"
+// "𝒫"
+// "𝒬"
+// "ℛ"
+// "𝒮"
+// "𝒯"
+// "𝒰"
+// "𝒱"
+// "𝒲"
+// "𝒳"
+// "𝒴"
+// "𝒵"
+// "𝒶"
+// "𝒷"
+// "𝒸"
+// "𝒹"
+// "ℯ"
+// "𝒻"
+// "ℊ"
+// "𝒽"
+// "𝒾"
+// "𝒿"
+// "𝓀"
+// "𝓁"
+// "𝓂"
+// "𝓃"
+// "ℴ"
+// "𝓅"
+// "𝓆"
+// "𝓇"
+// "𝓈"
+// "𝓉"
+// "𝓊"
+// "𝓋"
+// "𝓌"
+// "𝓍"
+// "𝓎"
+// "𝓕"
+// "1"
+// "2"
+// "3"
+// "4"
+// "5"
+// "6"
+// "7"
+// "8"
+// "9"
+// "0"
+
+// $(document).on("selectionchange", moveToolbar);
+
+// const moveToolbar = (e) => {
+//   const selection = document.getSelection();
+//   const coordinates = getSelectionCoordinates(selection);
+
+//   if (coordinates && selection.type === TYPE_RANGE) {
+//     const toolbarX = coordinates.width / 2 + coordinates.x;
+//     updateActionStatus(selection);
+
+//     $toolbar.style.setProperty("--top", `${coordinates.y}px`);
+//     $toolbar.style.setProperty("--left", `${toolbarX}px`);
+//     $toolbar.style.display = "block";
+//   } else {
+//     $toolbar.style.display = "none";
+//   }
+// };
+// const getSelectionCoordinates = (selection) => {
+//   const r = selection.getRangeAt(0);
+//   const clip = r.getClientRects();
+
+//   if (!clip.length) return false;
+
+//   const { x, y, width } = clip[0];
+//   return { x, y, width };
+// };
+
+// const updateActionStatus = (selection) => {
+//   const actions = ["bold"];
+//   const cssText = getCssText(selection);
+
+//   actions.forEach((action) => {
+//     const isActive = cssText.includes(action);
+//     toggleActionState(action, isActive);
+//   });
+// };
+
